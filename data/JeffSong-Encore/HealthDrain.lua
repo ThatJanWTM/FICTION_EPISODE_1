1ea6f860ff20ed44555c5c90ad95fb1096ed951d
ThatJan <jankarlo933@gmail.com>
2025-05-01 18:36:49 -0400

    JeffSong Changes

--- data/JeffSong-Encore/HealthDrain.lua
+++ data/JeffSong-Encore/HealthDrain.lua
@@ -1,6 +1,6 @@
 function opponentNoteHit()
     health = getProperty('health')
     if getProperty('health') > 0.05 then
-        setProperty('health', health- 0.001);
+        setProperty('health', health- 0.005);
     end
 end
