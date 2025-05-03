eba92a83bb3fc95d0e4d40ce8bf06e45d9631746
Blue <Mikey09607@gmail.com>
2025-05-03 16:42:58 -0400

    Changed health drain for Spectral Jeff
    hi jan 😄

--- characters/jeff_spectral.lua
+++ characters/jeff_spectral.lua
@@ -2,7 +2,7 @@ function opponentNoteHit()
 	if dadName == 'jeff_spectral' then
 		health = getProperty('health')
 		if getProperty('health') > 0.05 then
-		setProperty('health', health- 0.01);
+		setProperty('health', health- 0.007);
 		end
 	end
 end
